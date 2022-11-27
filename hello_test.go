2f174947c5e1bd6c61eb403f506771aad3ac0cfc
Alexkim <kyongki@38f9d3ac2780.ant.amazon.com>
2022-11-27 13:05:12 +0900

    3rd version

--- hello_test.go
+++ hello_test.go
@@ -2,11 +2,22 @@ package main
 
 import "testing"
 
-func testHello(t *testing.T) {
-	got := Hello("James")
-	want := "Hello, john"
+func TestHello(t *testing.T) {
+	t.Run("hello with name", func(t *testing.T) {
+		got := Hello("James")
+		want := "Hello, James"
+		assertCorrectMsg(t, got, want)
+	})
+	t.Run("hello with default", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, Everyone"
+		assertCorrectMsg(t, got, want)
+	})
+}
 
+func assertCorrectMsg(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
-		t.Errorf("got %q, want %q", got, want)
+		t.Errorf("got: %q, but want %q", got, want)
 	}
 }
